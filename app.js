c8dba9945d47d78eb34d5f0d4d22cb5f9bc976ef
108213050 <s108213050@mail1.ncnu.edu.tw>
2021-12-07 22:46:46 +0800

    create connection pool

--- app.js
+++ app.js
@@ -3,11 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { connect } = require('tls');
+//產生呼叫express物件
 
 var app = express();
+let db_config = {
+  database : 'todo',
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+}
+// const connection =mysql.createConnection(db_config);
+const connection =mysql.createPool(db_config);
+
+// connection.connect(function(err) {
+//   if (err) {
+//       console.log('connecting error');
+//       return;
+//   }
+//   console.log('connecting success');
+// });
+// 把 db 的連線綁定到 req 裡面
+// app.use 使用某個function
+app.use(function(req, res, next) {
+  req.db = connection;
+  next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,34 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-let db_config = {
-  database : 'todo',
-  host     : 'localhost',
-  user     : 'root',
-  password : ''
-}
-var connection = mysql.createConnection(db_config);
+// let db_config = {
+//   database : 'todo',
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : ''
+// }
+// var connection = mysql.createConnection(db_config);
+// 
 // var connection = mysql.createConnection({
 //   database : 'todo',
 //   host     : 'localhost',
 //   user     : 'root',
 //   password : ''
 // });
-connection.connect();
-// connection.end();
-
+// connection.connect();要用這個
+// connection.end();直接報廢
+// var connection = mysql.creatPool (db_config);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM user WHERE id=1', function(err, rows, fields) {
+  // 在app.js 把conneciton 塞到req.db => req.db = connection
+  req.db.query('SELECT * FROM user WHERE id=1', function(err, rows, fields) {
     if (err) throw err;
     console.log('SELECT Result: ', rows[0].id);
     console.log('SELECT Result: ', rows[0].username);
     console.log('SELECT PWD: ', rows[0].password);
     let data = {
-      title: '',
+      title: '軟工課輔',
       username : rows[0].username
     }
     res.render('index', data);  
